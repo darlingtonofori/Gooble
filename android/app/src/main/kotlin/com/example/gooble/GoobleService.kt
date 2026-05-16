4e6d81f68b0356eda184943f1789282189715ef4
TR4N5P4R3NT <159647064+darlingtonofori@users.noreply.github.com>
2026-05-16 09:59:43 +0000

    Add AccessibilityService import to GoobleService

--- android/app/src/main/kotlin/com/example/gooble/GoobleService.kt
+++ android/app/src/main/kotlin/com/example/gooble/GoobleService.kt
@@ -13,6 +13,7 @@ import org.json.JSONArray
 import org.json.JSONObject
 import java.net.HttpURLConnection
 import java.net.URL
+import android.accessibilityservice.AccessibilityService
 
 class GoobleService : Service() {
 
